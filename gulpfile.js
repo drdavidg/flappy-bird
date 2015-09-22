e6951b71fc8fc94f04dfc806ef40930686d06c9c
David Gonen <david@curiousminds.com>
2015-09-22 10:23:29 +0300

    trying to get browserify working in gulp.  still getting 'require is not defined' browser error

--- gulpfile.js
+++ gulpfile.js
@@ -14,34 +14,30 @@ var minifyCSS = require('gulp-cssmin');
 
 //Javascript linting task
 gulp.task('jshint', function() {
-	return gulp.src('js/*.js') //TODO RYAN: how do i include js files in subdirectories also?
+	return gulp.src(['js/*.js', 'js/components/graphics/*.js', 'js/entities/*.js', 'js/systems/*.js' ]) //TODO RYAN: how do i include js files in subdirectories also?
 		.pipe(jshint())
 		.pipe(jshint.reporter('default'));
 });
-// Compile Sass task
-// gulp.task('sass', function() {
-//   return gulp.src('site/scss/*.scss')
-//     .pipe(sass())
-//     .pipe(gulp.dest('site/css'));
-// });
 // Watch task
 gulp.task('watch', function() {
   gulp.watch('site/js/*.js', ['jshint']);//TODO RYAN: how do i include js files in subdirectories also?
-  gulp.watch('site/scss/*.scss', ['sass']);
+	gulp.watch('js/components/graphics/*.js', ['jshint']);
+	gulp.watch('js/entities/*.js', ['jshint']);
+	gulp.watch('js/systems/*.js', ['jshint']);
 });
 // Default task
-gulp.task('default', ['jshint', 'sass', 'watch']);
+gulp.task('default', ['jshint', 'watch']);
 
 // Minify index
 gulp.task('html', function() {
-  gulp.src('site/index.html')
+  gulp.src('index.html')
     .pipe(minifyHTML())
     .pipe(gulp.dest('build/'));
 });
 
 // JavaScript build task, removes whitespace and concatenates all files
 gulp.task('scripts', function() {
-  return browserify('./site/js/main.js')
+  return browserify('./js/main.js')
     .bundle()
     .pipe(source('app.js'))
     .pipe(buffer())
@@ -50,18 +46,18 @@ gulp.task('scripts', function() {
 });
 // Styles build task, concatenates all the files
 gulp.task('styles', function() {
-  gulp.src('site/css/*.css')
+  gulp.src('css/*.css')
 		.pipe(minifyCSS())
 		.pipe(concat('styles.css'))
     .pipe(gulp.dest('build/css'));
 });
 // Image optimization task
 gulp.task('images', function() {
-  gulp.src('site/img/*')
+  gulp.src('img/*')
     .pipe(imagemin())
     .pipe(gulp.dest('build/img'));
 });
 //minify css
 
 // Build task
-gulp.task('build', ['jshint', 'sass', 'html', 'scripts', 'styles', 'images']);
+gulp.task('build', ['jshint', 'html', 'scripts', 'styles', 'images']);
